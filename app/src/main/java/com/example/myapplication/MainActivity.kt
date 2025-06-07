fe41f8ea8727c79203739f65228ae15cf949ec21
OpenScriptsOfficial <aleksejspivak31@gmail.com>
2025-06-07 22:10:38 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener{
+
+        }
+    }
+}
